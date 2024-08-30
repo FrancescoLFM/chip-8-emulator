98fce641cb1609b3c9bbf4282d1ec1c56851c3ef
FrancescoLFM <speedartemmekappose@gmail.com>
2024-08-30 13:54:58 +0200

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use emulator::decoder::Decoder;
+use emulator::{decoder::Decoder, fetcher::Fetcher};
+use std::fs::File;
 
 pub mod window;
-pub mod emulator {
-    pub mod decoder;
-}
-
+mod emulator;
 fn main() {
+    let mut fetcher = Fetcher::new();
+    fetcher.load_rom_mem("roms/Hello_World.ch8");
+    let first_instruction = fetcher.fetch_instruction();
+    assert_eq!(first_instruction, 0x1202);
     let decoder = Decoder::new();
     if let Some(instr) = decoder.simple_instruction_map.get(&0x00E0) {
         instr.execute();
